dd6bcdf52a51bb5104db25ac2bfff91bfabc900c
Boim <ibrahim98208@gmail.com>
2026-03-26 18:43:11 -0700

    implemented natural fretting engine

--- Scripts/ScriptProcessors/AmbersandGuitar_Main/FrettingEngine.js
+++ Scripts/ScriptProcessors/AmbersandGuitar_Main/FrettingEngine.js
@@ -1,8 +1,14 @@
  const var NUMOFSTRINGS = Globals.NUMOFSTRINGS;
  const var LOWESTNOTE = 52;
  const var HIGHESTNOTE = 88;
-
-
+ 
+ const var OPENSTRING6NOTE = 52;
+ const var OPENSTRING5NOTE = 57;
+ const var OPENSTRING4NOTE = 62;
+ const var OPENSTRING3NOTE = 67;
+ const var OPENSTRING2NOTE = 71;
+ const var OPENSTRING1NOTE = 76;
+ 
  
  namespace Stringtype
  {
@@ -44,8 +50,12 @@ for(var i = 0; i < NUMOFSTRINGS; i++){
 	stringNote.push(-1);
 }
 
+var handPositionFret = 0;
+//this is where the leftmost part of the virtual guitarist's hand is
 
-//ensure only one sampler plays a voice at a time
+
+
+//functions to ensure only one sampler plays a voice at a time
 
  inline function playString6(){
     string6Mute.setAttribute("Bypass", false);
@@ -101,6 +111,11 @@ string1Mute.setAttribute("Bypass", true);
  string1Mute.setAttribute("Bypass", false);
  }
  
+ inline function isBetweenIncl(num, lowBound, highBound){
+ 	 return num >= lowBound && num <= highBound;
+ }
+ 
+
  
  inline function updateGlobals(){
 	 Globals.stringNote1 = stringNote[Stringtype.STRING1];
@@ -111,6 +126,100 @@ string1Mute.setAttribute("Bypass", true);
 	 Globals.stringNote6 = stringNote[Stringtype.STRING6];
  }
  
+ 
+ 
+ inline function primitiveFretting(notePlayed){
+	 
+//fretting engine designed for going low to high string then going back down
+//use primarily for quick debugging 
+ 
+	 if (stringNote[Stringtype.STRING6] == -1){
+	 	playString6();
+	 	stringNote[Stringtype.STRING6] = notePlayed;
+	 	updateGlobals();
+	 	return;
+	 }else if (stringNote[Stringtype.STRING5] == -1){
+	 	playString5();
+	 	stringNote[Stringtype.STRING5] = notePlayed;
+	 	updateGlobals();
+	 	return;
+	 }else if (stringNote[Stringtype.STRING4] == -1){
+	 	playString4();
+	 	stringNote[Stringtype.STRING4] = notePlayed;
+	 	updateGlobals();
+	 	return;
+	 }else if (stringNote[Stringtype.STRING3] == -1){
+	 	playString3();
+	 	stringNote[Stringtype.STRING3] = notePlayed;
+	 	updateGlobals();
+	 	return;
+	 }else if (stringNote[Stringtype.STRING2] == -1){
+	 	playString2();
+	 	stringNote[Stringtype.STRING2] = notePlayed;
+	 	updateGlobals();
+	 	return;
+	 }else if (stringNote[Stringtype.STRING1] == -1){
+	 	playString1();
+	 	stringNote[Stringtype.STRING1] = notePlayed;
+	 	updateGlobals();
+	 	return;
+	 }
+	 
+	 
+ }
+ 
+ 
+ inline function naturalFretting(notePlayed, handPositionFret){
+	 //the guitarist will only play as close as to their hands naturally are. 
+	 
+	 Console.print(handPositionFret);
+	 Console.print(notePlayed);
+	 
+	 if(!isBetweenIncl(notePlayed, LOWESTNOTE, HIGHESTNOTE)){
+		 return handPositionFret;
+	 }
+	 if(isBetweenIncl(notePlayed, OPENSTRING6NOTE, OPENSTRING6NOTE + 4 + handPositionFret)){
+		 playString6();
+		 stringNote[Stringtype.STRING6] = notePlayed;
+		 updateGlobals();
+		 
+		 if(notePlayed < handPositionFret + OPENSTRING6NOTE){
+			 return notePlayed - OPENSTRING6NOTE;
+		 }
+		 
+		 return handPositionFret;
+	 }else if(isBetweenIncl(notePlayed, OPENSTRING5NOTE + handPositionFret, OPENSTRING5NOTE + 4 + handPositionFret)){
+		 playString5();
+		 stringNote[Stringtype.STRING5] = notePlayed;
+		 updateGlobals();
+		 return handPositionFret;
+	 }else if(isBetweenIncl(notePlayed, OPENSTRING4NOTE + handPositionFret, OPENSTRING4NOTE + 4 + handPositionFret)){
+		 playString4();
+		 stringNote[Stringtype.STRING4] = notePlayed;
+		 updateGlobals();
+		 return handPositionFret;
+	 }else if(isBetweenIncl(notePlayed, OPENSTRING3NOTE + handPositionFret, OPENSTRING3NOTE + 3 + handPositionFret)){
+		 playString3();
+		 stringNote[Stringtype.STRING3] = notePlayed;
+		 updateGlobals();
+		 return handPositionFret;
+	 }else if(isBetweenIncl(notePlayed, OPENSTRING2NOTE + handPositionFret, OPENSTRING2NOTE + 4 + handPositionFret)){
+		 playString2();
+		 stringNote[Stringtype.STRING2] = notePlayed;
+		 updateGlobals();
+		 return handPositionFret;
+	 }else{
+		 playString1();
+		 stringNote[Stringtype.STRING1] = notePlayed;
+		 updateGlobals();
+		 if(notePlayed - handPositionFret < OPENSTRING1NOTE + 5){
+			 return handPositionFret;
+		 }else{
+			 return notePlayed - OPENSTRING1NOTE - 4;
+		 }
+	 }
+	 
+ }
  
  
  inline function isEventStillPlaying(eventId)
@@ -133,43 +242,8 @@ string1Mute.setAttribute("Bypass", true);
 
 
 
-	if (stringNote[Stringtype.STRING6] == -1){
-		Console.print("playing string 6");
-		playString6();
-		stringNote[Stringtype.STRING6] = notePlayed;
-		updateGlobals();
-		return;
-	}else if (stringNote[Stringtype.STRING5] == -1){
-		Console.print("playing string 5");
-		playString5();
-		stringNote[Stringtype.STRING5] = notePlayed;
-		updateGlobals();
-		return;
-	}else if (stringNote[Stringtype.STRING4] == -1){
-		Console.print("playing string 4");
-		playString4();
-		stringNote[Stringtype.STRING4] = notePlayed;
-		updateGlobals();
-		return;
-	}else if (stringNote[Stringtype.STRING3] == -1){
-		Console.print("playing string 3");
-		playString3();
-		stringNote[Stringtype.STRING3] = notePlayed;
-		updateGlobals();
-		return;
-	}else if (stringNote[Stringtype.STRING2] == -1){
-		Console.print("playing string 2");
-		playString2();
-		stringNote[Stringtype.STRING2] = notePlayed;
-		updateGlobals();
-		return;
-	}else if (stringNote[Stringtype.STRING1] == -1){
-		Console.print("playing string 1");
-		playString1();
-		stringNote[Stringtype.STRING1] = notePlayed;
-		updateGlobals();
-		return;
-	}
+	handPositionFret = naturalFretting(notePlayed, handPositionFret);
+	
 	
 
 	
