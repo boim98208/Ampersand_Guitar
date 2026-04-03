f1b81e86593bbdea7bf975895004ad773c1370b4
Boim <ibrahim98208@gmail.com>
2026-04-03 12:50:54 -0700

    remiced samplemaps and cleaned up channel routing

---
+++ Scripts/ScriptProcessors/AmbersandGuitar_TestingMicdSamples/Container0String3ChnlRoute.js
@@ -0,0 +1,36 @@
+ function onNoteOn()
+{
+
+	if(Message.getChannel() != 3){
+		
+		Message.ignoreEvent(true);
+		Globals.string3ActiveRR = "not playing";
+	}else{
+		
+	
+		//is now playing the note and updates	
+		Globals.string3ActiveRR = Sampler.getActiveRRGroup();
+	}
+}
+ function onNoteOff()
+{
+	if(Message.getChannel() != 3){
+		Message.ignoreEvent(true);
+	}else{
+	
+		Globals.string3ActiveRR = "not playing";
+	}
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
